7e9fbb7f171121f1a646c1c6b698ba4b19453cfc
kylevelarde <52535645+TechGitMaster@users.noreply.github.com>
2023-07-03 16:46:30 +0800

    change last

--- server/index.js
+++ server/index.js
@@ -9,7 +9,7 @@ const App = require('./App');
 //1. This is just for second option if header in vercel.json have an error when uploading to vercel________________________
 //2. Also delete "headers" in vercel.json if have an error when uploading to vercel________________________
 //this is only for local___________________________________
-const cors = require('cors');
+//const cors = require('cors');
 /*app.use(cors({
     origin: ['http://localhost:3000', 'https://thesisary.vercel.app']
 }));*/
