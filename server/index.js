e1aa861a9fd1b3fa6c6d905eedde7520da6bca64
kylevelarde <52535645+TechGitMaster@users.noreply.github.com>
2023-07-03 16:02:37 +0800

    update for server

--- server/index.js
+++ server/index.js
@@ -8,8 +8,10 @@ const App = require('./App');
 
 //This is just for second option if header in vercel.json have an error when uploading to vercel________________________
 //this is only for local___________________________________
-/*const cors = require('cors');
-app.use(cors());*/
+const cors = require('cors');
+app.use(cors({
+    origin: ['http://localhost:3000', 'https://thesisary.vercel.app']
+}));
 
 
 // Set up options response for preflight requests
